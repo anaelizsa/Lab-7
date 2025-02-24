0e617b9fdf743f615ec990d188d230c8d09f8503
anakaren.godinezp@gmail.com <anakaren.godinez@gmail.com>
2025-02-24 12:47:01 -0600

    Footer

---
+++ Lab 7/script.js
@@ -0,0 +1,10 @@
+const footerHTML = `
+  <footer>
+    <p><strong>Autores:</strong> Ana Karen Godinez, Ana Paola Elizondo, Montse Pineda, Estefanía León, Diana Caballero, Daniela Contreras, Tamara Fernández</p>
+    <p>contacto@whitelotus.com</p>
+    <p>White Lotus</p>
+    <img src="./Pic/whitelotus.jpg" style="width: 250px; height: auto; margin-top: 10px;">
+  </footer>
+`;
+
+document.body.insertAdjacentHTML('beforeend', footerHTML);
