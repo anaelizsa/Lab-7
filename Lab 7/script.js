56c0768f7340c89ef09fc95ea4adb486bf6877e1
Ana Elizondo-Salas <anaelizondo796@gmail.com>
2025-02-26 13:19:29 -0600

    Contact us update
    Cambios en contact us

--- Lab 7/script.js
+++ Lab 7/script.js
@@ -3,3 +3,22 @@ fetch("menu.html") //Se llama al menu
 .then(response => response.text())//Se convierte la info de menu en texto legible
 .then(data => document.getElementById("menuInfo").innerHTML = data); //busca el elemento vacio en index e inserta la info del documento html
 
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Tu mensaje fue enviado!', 'info')
+  })
+}
